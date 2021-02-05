23423123f079dd633f980d89db643fa26c494c63
shendl1978 <sdl55823273>
2021-02-05 18:19:52 +0800

    Changes to be committed: 	modified:   index.ts 	modified:   package-lock.json 	modified:   package.json

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
+//import * as process  from "process"
 export const name='quick-helper'
-
 
 export function numberToFixNumber(val:number,length:number){
     let fixNumber=``
@@ -41,8 +41,91 @@ export function datetimeToIsoformat(date:Date){
     }
     return dateStr
 }
+// export  function transSpecialChar(pageStr) {
+//     if (pageStr != undefined && pageStr != "" && pageStr != 'null') {
+//         pageStr = pageStr.replace(/\r/g, "\\r");
+//         pageStr = pageStr.replace(/\n/g, "\\n");
+//         pageStr = pageStr.replace(/\t/g, "\\t");
+//         pageStr = pageStr.replace(/\\/g, "\\\\");
+//         pageStr = pageStr.replace(/"\[{/g, "[{");
+//         pageStr = pageStr.replace(/}]"/g, "}]");
+//         // pageStr = pageStr.replace(/("")+/g, '"');
+//         pageStr = pageStr.replace(/"{"/g, "{\"");
+//         pageStr = pageStr.replace(/"}"/g, "\"}");
+//         pageStr = pageStr.replace(/}}"/g, "}}");
+//         pageStr = pageStr.replace(/\'/g, "&#39;");
+//         pageStr = pageStr.replace(/ /g, "&nbsp;");
+//         pageStr = pageStr.replace(/</g, "$lt;");
+//         pageStr = pageStr.replace(/>/g, "$gt;");
+//     }
+//     return pageStr;
+// }
+// export  function fixForJSONstringify(pageStr:string) {
+//     if (pageStr != undefined && pageStr != "" && pageStr != 'null') {
+//         pageStr = pageStr.replace(/"{/g, "{");
+//         pageStr = pageStr.replace(/}"/g, "}");
+//         pageStr = pageStr.replace(/\\"/g, `"`);
+       
+       
+//     }
+//     return pageStr;
+// }
+//let myLog=jsonLog({logApp:"myApp",logName:"structLog-start",logMsg:{"name":"shendl",'age':42}})
+export function jsonLog({logApp,logName,logMsg,logVersion='1.0.0',logLevel='info'}:{
+    logApp:string,logName:string,logMsg:any,logVersion?:string,logLevel?:string
+}){
+    let userAgent=''
+    let  browserLanguage=''
+    let referrer= '' 
+    // if(process.browser){
+    //     userAgent=navigator.userAgent
+    //        browserLanguage=navigator.language
+    //        referrer= document.referrer
+    // }else{
+        
+    // }
+    if(typeof navigator !== "undefined" && navigator !== null){
+          userAgent=navigator.userAgent
+           browserLanguage=navigator.language
+    }
+    if(typeof document !== "undefined" && document !== null){
+          referrer= document.referrer
+ 
+    }
+    let logObj={
+        "logLevel":logLevel,
+
+        "logTime":datetimeToIsoformat(new Date()),
+        "logApp":logApp,
+        "logName":logName,
+        "logVersion":logVersion,
+        "userAgent": userAgent,
+       
+       "browserLanguage": browserLanguage,
+       "referrer": referrer,
+        "logMsg":logMsg,
+
+
+    }
+    const logStr=JSON.stringify(logObj)
+    console.log(logStr)
+    return logStr;
+
+}
 
 //test
+jsonLog({logApp:"myApp",logName:"structLog-start",logMsg:123})
+jsonLog({logApp:"myApp",logName:"structLog-End",logMsg:'Hello world!'})
+let myLog=jsonLog({logApp:"myApp",logName:"structLog-start",logMsg:{"name":"shendl",'age':42}})
+console.log(`1:${myLog}`)
+//  myLog=fixForJSONstringify(myLog)
+//  console.log(`2:${myLog}`)
+ let myObj=JSON.parse(myLog)
+ console.log(`3:${myObj}`)
+ console.log(`4:${JSON.stringify(myObj)}`)
+
+
+
 // let myStr=numberToFixNumber(123456,6)
 // console.log(myStr)
 // let myDate=new Date()
